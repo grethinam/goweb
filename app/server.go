0f6e55d98e8046b563d184ce9f85fa5c912888db
Gopinath Rethinam <gopinathasku@gmail.com>
2018-05-24 17:12:05 +0200

    adding hostname function

--- app/server.go
+++ app/server.go
@@ -3,10 +3,12 @@ package main
 import (
     "fmt"
     "net/http"
-    "os"
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    //"os"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
+/*func helloWorld(w http.ResponseWriter, r *http.Request){
     name, err := os.Hostname()
 
     if err != nil {
@@ -15,9 +17,39 @@ func helloWorld(w http.ResponseWriter, r *http.Request){
     
     fmt.Fprintf(w, "Hello World!!!!")
     fmt.Fprintf(w, name)
+}*/
+
+func dbTable(w http.ResponseWriter, r *http.Request){
+    db, err := sql.Open("mysql", "root:supersecret@tcp(35.192.3.225:3306)/company?charset=utf8")
+	checkErr(err)
+	rows, err := db.Query("select * from employees")
+	checkErr(err)
+	
+	for rows.Next() {
+		var first_name string
+		var last_name string
+		var department string
+		var email string
+		err = rows.Scan(&first_name, &last_name, &department, &email)
+		checkErr(err)
+		fmt.Fprintf(first_name)
+		fmt.Fprintf(last_name)
+		fmt.Fprintf(department)
+		fmt.Fprintf(email)
+	}
+	
+	db.Close()
+
 }
 
 func main() {
-    http.HandleFunc("/", helloWorld)
+    //http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/", dbTable)
     http.ListenAndServe(":8080", nil)
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
