7b96a31c873624216b05e060d92696ecb243641d
Gopinath Rethinam <gopinathasku@gmail.com>
2018-05-25 11:51:59 +0200

    adding format for output

--- app/server.go
+++ app/server.go
@@ -32,10 +32,10 @@ func dbTable(w http.ResponseWriter, r *http.Request){
 		var email string
 		err = rows.Scan(&first_name, &last_name, &department, &email)
 		checkErr(err)
-		fmt.Fprintf(w, first_name)
-		fmt.Fprintf(w, last_name)
+		fmt.Fprintf(w,"|%6s|%6s|%6s|%6s|" ,first_name ,last_name ,department ,email)
+		/*fmt.Fprintf(w, last_name)
 		fmt.Fprintf(w, department)
-		fmt.Fprintf(w, email)
+		fmt.Fprintf(w, email)*/
 	}
 	
 	db.Close()
