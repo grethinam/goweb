3f2255101a61d344a0ad1501b63ba527738ea062
Gopinath Rethinam <gopinathasku@gmail.com>
2018-06-09 01:26:22 +0530

    change layout and go

--- app/server.go
+++ app/server.go
@@ -65,7 +65,7 @@ func dbTable(w http.ResponseWriter, r *http.Request){
     table := dbSelect()
 	for i := range(table) {
         emp := table[i]
-        fmt.Fprintf(w,"YESS|%12s|%12s|%12s|%20s|\n" ,emp.fname ,emp.sname ,emp.dname ,emp.email)
+        fmt.Fprintf(w,"YESSS|%12s|%12s|%12s|%20s|\n" ,emp.fname ,emp.sname ,emp.dname ,emp.email)
     }
 }
 
